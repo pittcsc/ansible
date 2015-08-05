2c00d87530ee4841aeacb489c4bbec4112f6c118
George Claghorn <george.claghorn@gmail.com>
2015-08-05 00:01:28 -0400

    Slackin

---
+++ roles/slackin/app/templates/slackin.eye.rb
@@ -0,0 +1,15 @@
+Eye.application('slackin') do
+  uid 'slackin'
+  gid 'slackin'
+  working_dir '/home/slackin'
+
+  process 'slackin' do
+    start_command 'slackin -p "{{ slackin_port }}" -s "{{ slackin_subdomain }}" "{{ slackin_api_token }}"'
+
+    start_grace 5.seconds
+    stop_grace 5.seconds
+    restart_grace 10.seconds
+
+    daemonize true
+  end
+end
